81b7fe90951398cb602c37b8b02bbe3a977be37d
Nitish Parkar <developer.nitish@gmail.com>
2022-09-15 12:31:16 +0530

    Add Stimulus demo code
    https://stimulus.hotwired.dev/handbook/hello-stimulus

---
+++ app/controllers/stimulus_demo_controller.rb
@@ -0,0 +1,2 @@
+class StimulusDemoController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :rooms do
     resources :messages
   end
+
+  get '/stimulus_demo', to: 'stimulus_demo#example'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
